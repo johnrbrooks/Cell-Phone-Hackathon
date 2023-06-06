636315896cabaebfd5df06468a76bfb3b1f08619
LowellC86 <lowellcaraballo@gmail.com>
2023-06-06 16:38:19 -0400

    iphones

---
+++ seed/index.js
@@ -0,0 +1,38 @@
+const db = require('../db')
+const { Phones } = require('../models')
+
+// Connect to the database
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+
+
+    const phones = [
+        { model_id: '', 
+        name: "iPhone 14 Pro", 
+        generation: 14, 
+        release_yr: 2023,
+        price: 999,
+        storage: "128GB",
+        screen_size: 6.7,
+        cx_rating: 4.6,
+        connector_type:
+        camera_qual: "48MP",
+        ext_storage: False,
+        platform_id:
+        make_id: 
+    }
+       
+        
+
+]
+
+    await Phones.insertMany(movies)
+    console.log("Check these phones out!")
+}
+const run = async () => {
+    await main()
+    db.close()
+}
+
+run()
