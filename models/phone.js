94be71f851e58677a3050ebab329f4f2d2bd6be5
John Brooks <johnrbrooks93@gmail.com>
2023-06-07 14:07:23 -0400

    Seeded once again

--- models/phone.js
+++ models/phone.js
@@ -3,20 +3,19 @@ const { Schema } = require('mongoose')
 const phoneSchema = new Schema(
     {
         image: {type: String, required: true },
-        model_id: { type: String, required: true },
         name: { type: String, required: true },
         generation: { type: String, required: true },
         release_yr: { type: String, required: true },
         price: { type: Number, required: true },
-        storage: { type: String, required: true },
+        storage: [{ type: String, required: true }],
         screen_size: { type: String, required: true },
         cx_rating: { type: String, required: true },
         connector_type: { type: String, required: true },
         camera_qual: { type: String, required: true },
         ext_storage: { type: Boolean, required: true },
         connector_type: { type: String, required: true },
-        platform_id: { type: Schema.Types.ObjectId, ref: 'platform_id' },
-        make_id: { type: Schema.Types.ObjectId, ref: 'make_id' }
+        platform_id: { type: String, required: true },
+        make_id: { type: String, required: true }
     }
 )
 
--- models/platform.js
+++ models/platform.js
@@ -3,7 +3,6 @@ const { Schema } = require('mongoose')
 const platformSchema = new Schema(
     {
         platform_name: { type: String, required: true },
-
     }
 )
 
